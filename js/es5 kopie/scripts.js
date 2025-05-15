9852725bcc7f537e5fdcd87bea59c6e584842335
arnoud <arnoud@frontendstudio.com>
2025-05-15 14:37:01 +0200

    cleanup

--- js/es5 kopie/scripts.js
+++
@@ -1,9 +0,0 @@
-/**
- * cmsms_uisge-beatha
- * @version v1.0.0
- * @link https://github.com/UisgeBeatha436/cmsms_uisge-beatha
- * @author Gregor
- * @license GPL-3.0
- */
-var rellax=new Rellax(".rellax",{callback:function(l){console.log(l)}});
-//# sourceMappingURL=scripts.js.map
