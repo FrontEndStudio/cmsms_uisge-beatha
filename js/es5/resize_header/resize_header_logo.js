08f13d15c510b2e08e7e1866bfcef337e34c33c2
arnoud <arnoud@frontendstudio.com>
2025-05-15 16:01:35 +0200

    fix linter errors: ~ npm run testJSES5

--- js/es5/resize_header/resize_header_logo.js
+++ js/es5/resize_header/resize_header_logo.js
@@ -1,19 +1,24 @@
-    window.onscroll = function() {
-    growShrinkLogo()
-  };
+window.onscroll = function() {
+  growShrinkLogo();
+};
 
-  var Logo = document.getElementById("header_logo");
-  var endOfDocumentTop = 150;
-  var size = 0;
+var Logo = document.getElementById('header_logo');
+var endOfDocumentTop = 150;
+var size = 0;
 
-  function growShrinkLogo() {
-  var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+/**
+ * @todo document
+ */
+function growShrinkLogo() {
+  var scroll =
+    window.pageYOffset ||
+    document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-    if (size == 0 && scroll > endOfDocumentTop) {
-      Logo.className = 'small-logo';
-      size = 1;
-    } else if(size == 1 && scroll <= endOfDocumentTop){
-      Logo.className = 'large-logo';
+  if (size == 0 && scroll > endOfDocumentTop) {
+    Logo.className = 'small-logo';
+    size = 1;
+  } else if (size == 1 && scroll <= endOfDocumentTop) {
+    Logo.className = 'large-logo';
     size = 0;
-    }
   }
+}
--- js/es5/scripts/rellax_parameters.js
+++ js/es5/scripts/rellax_parameters.js
@@ -1,8 +1,9 @@
 // Parallax script
+// eslint-disable-next-line no-unused-vars
 var rellax = new Rellax('.rellax', {
 // center: true
-callback: function(position) {
+  callback: function(position) {
     // callback every position change
     console.log(position);
-}
+  },
 });
